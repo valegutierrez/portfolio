97f0bae7a0f64dd139d7604ff9faf1f0a6e69adf
valegutierrez <vale.gutierrezc@gmail.com>
2025-04-02 00:53:49 -0300

    feat: navbar & footer styled

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,28 @@
 import { useEffect, useState } from "react";
 import Logo from "../../../public/logo vale ux.svg"
+import { NavLink } from "react-router-dom";
 
-function Navbar() {
+interface NavbarProps {
+  secondary?: boolean;
+}
+
+function Navbar(props: NavbarProps) {
+  const { secondary } = props;
   const [isLargeViewport, setIsLargeViewport] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [scroll, setScroll] = useState(false);
   
   useEffect(() => {
     const handleResize = () => {
       setIsLargeViewport(window.innerWidth >= 768);
+      setIsMenuOpen(false);
     };
 
-    handleResize(); // Set initial state
+    handleResize();
     window.addEventListener("resize", handleResize);
+    window.addEventListener("scroll", () => {
+      setScroll((isLargeViewport && window.scrollY > 650) || (!isLargeViewport && window.scrollY > 480))
+    });
 
     return () => {
       window.removeEventListener("resize", handleResize);
@@ -24,23 +35,25 @@ function Navbar() {
   }
 
   return (
-    <nav className="navbar">
-      <img src={Logo} className="logo" />
-      {isLargeViewport ?
-        <ul className="navbar-links">
-          <li className="navbar-link">Acerca de mí</li>
-          <li className="navbar-link">Portafolio</li>
-          <li className="navbar-link">Contacto</li>
-        </ul>
-      : 
-        <i className={`hamburger-menu ri-${isMenuOpen ? "close-large" : "menu"}-line`} onClick={handleMenuToggle}></i>
-      }
-      <ul className={`navbar-mobile-links ${isMenuOpen ? "" : "display-none"}`}>
-        <li className="navbar-link">Acerca de mí</li>
-        <li className="navbar-link">Portafolio</li>
+    <>
+      <nav className={`navbar ${scroll || secondary ? "bg-lilac" : ""}`}>
+        <img src={Logo} className="logo" />
+        {isLargeViewport ?
+          <ul className="navbar-links">
+            <li className="navbar-link"><NavLink className={({ isActive }) => ( isActive ? "active" : "")} to="/#about-me">Acerca de mí</NavLink></li>
+            <li className="navbar-link"><NavLink className={({ isActive }) => ( isActive ? "active" : "")} to="/portfolio">Portafolio</NavLink></li>
+            <li className="navbar-link">Contacto</li>
+          </ul>
+        : 
+          <i className={`hamburger-menu ri-${isMenuOpen ? "close-large" : "menu"}-line`} onClick={handleMenuToggle}></i>
+        }
+      </nav>
+      <ul className={`navbar-mobile-links ${scroll || secondary ? "bg-lilac" : ""} ${isMenuOpen ? "open" : ""}`}>
+        <li className="navbar-link"><NavLink className={({ isActive }) => ( isActive ? "active" : "")} to="/#about-me">Acerca de mí</NavLink></li>
+        <li className="navbar-link"><NavLink className={({ isActive }) => ( isActive ? "active" : "")} to="/portfolio">Portafolio</NavLink></li>
         <li className="navbar-link">Contacto</li>
       </ul>
-    </nav>
+    </>
   )
 };
 export default Navbar;
